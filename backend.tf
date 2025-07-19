d0969452098be26e6917a2fb5d8c3f21c829fb35
GabrielHespanhol <gabriellopeshespanhol@gmail.com>
2025-07-18 22:57:58 -0300

    feat: Ajuste no backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 # Feito a configuração do backend remoto com state lock diretamente via S3
 terraform {
   backend "s3" {
-    bucket       = "linuxtips-devops-professional"
+    bucket       = "bucket_name_here"
     key          = "module-root/root.tfstate"
     region       = "us-west-2"
     use_lockfile = true
